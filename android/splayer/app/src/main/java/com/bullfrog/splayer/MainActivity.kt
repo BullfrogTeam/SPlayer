25488e2b46560ac904168386ae3c4f23625b35f4
biezhihua <biezhihua@gmail.com>
2018-12-09 15:40:04 +0800

    android: rename splayer module

--- android/splayer/app/src/main/java/com/bullfrog/splayer/MainActivity.kt
+++ android/splayer/app/src/main/java/com/bullfrog/splayer/MainActivity.kt
@@ -2,11 +2,26 @@ package com.bullfrog.splayer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Example of a call to a native method
+        sample_text.text = stringFromJNI()
+    }
+
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
     }
 }
