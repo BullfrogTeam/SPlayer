854f378905c931a03a127af08ebbc5fe7a4902d8
biezhihua <biezhihua@gmail.com>
2019-01-18 23:58:25 +0800

    feat: jni onload

---
+++ splayer/splayer-native/src/main/cpp/global_player_field.h
@@ -0,0 +1,18 @@
+//
+// Created by biezhihua on 2019/1/18.
+//
+
+#ifndef SPLAYER_GLOBALPLAYERFIELD_H
+#define SPLAYER_GLOBALPLAYERFIELD_H
+
+#include <sys/types.h>
+#include <jni.h>
+
+class global_player_field {
+public:
+    pthread_mutex_t mutex;
+    jclass clazz;
+    JavaVM *jvm;
+};
+
+#endif //SPLAYER_GLOBALPLAYERFIELD_H
--- splayer/splayer-native/src/main/cpp/splayer_jni.cpp
+++ splayer/splayer-native/src/main/cpp/splayer_jni.cpp
@@ -4,44 +4,35 @@
 
 #include <jni.h>
 #include <android/log.h>
+#include <assert.h>
+#include <pthread.h>
+#include "global_player_field.h"
 
 static const char *kTAG = "splayer_jni";
 
-#define LOGI(...) \
-  ((void)__android_log_print(ANDROID_LOG_INFO, kTAG, __VA_ARGS__))
-#define LOGW(...) \
-  ((void)__android_log_print(ANDROID_LOG_WARN, kTAG, __VA_ARGS__))
-#define LOGE(...) \
-  ((void)__android_log_print(ANDROID_LOG_ERROR, kTAG, __VA_ARGS__))
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, kTAG, __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, kTAG, __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, kTAG, __VA_ARGS__))
+
+static global_player_field *global_player_field = new global_player_field();
 
 extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
-//    JNIEnv* env = NULL;
-//
-//    g_jvm = vm;
-//    if ((*vm)->GetEnv(vm, (void**) &env, JNI_VERSION_1_4) != JNI_OK) {
-//        return -1;
-//    }
-//    assert(env != NULL);
-//
-//    pthread_mutex_init(&g_clazz.mutex, NULL );
-//
-//    // FindClass returns LocalReference
-//    IJK_FIND_JAVA_CLASS(env, g_clazz.clazz, JNI_CLASS_IJKPLAYER);
-//    (*env)->RegisterNatives(env, g_clazz.clazz, g_methods, NELEM(g_methods) );
-//
-//    ijkmp_global_init();
-//    ijkmp_global_set_inject_callback(inject_callback);
-//
-//    FFmpegApi_global_init(env);
-    LOGE("JNI_OnLoad");
+    JNIEnv *env = NULL;
+
+    global_player_field->jvm = vm;
+    if (vm->GetEnv((void **) &env, JNI_VERSION_1_4) != JNI_OK) {
+        return -1;
+    }
+    assert(env != NULL);
+
+    pthread_mutex_init(&global_player_field->mutex, NULL);
 
     return JNI_VERSION_1_4;
 }
 
 extern "C" JNIEXPORT void JNI_OnUnload(JavaVM *jvm, void *reserved) {
-//    ijkmp_global_uninit();
 
-//    pthread_mutex_destroy(&g_clazz.mutex);
+    pthread_mutex_destroy(&global_player_field->mutex);
 
     LOGE("JNI_OnUnload");
 }
